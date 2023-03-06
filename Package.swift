6fda92871c6cf2a2054111ed0fdfef4699f775bb
Naresh Babu Kommana <naresh.kommana@onmobile.com>
2023-03-06 15:35:27 +0530

    Update Package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "OnMobileGamification",
-            type: .dynamic,
             targets: ["OnMobileGamificationPackage"]),
     ],
     targets: [
