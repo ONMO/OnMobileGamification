657e3d0e1ef496084fb7f5f1d6bcba87e032c270
Naresh OnMobile's <nareshkommana@users.noreply.github.com>
2025-12-05 23:28:25 +0530

    Remove OnMobileGamificationAPISDK target
    Removed OnMobileGamificationAPISDK target from Package.swift.

--- Package.swift
+++ Package.swift
@@ -27,15 +27,6 @@ let package = Package(
             url: "https://github.com/ONMO/OnMobileGamification/releases/download/1.0.16/OnMobileGamificationSDK.xcframework.zip",
             checksum: "f22e4522b450c3dc9518827ff4d1f41276b8ca9c64f3ecf3c06be1e6fde46916"
         ),
-        .target(
-            name: "OnMobileGamificationAPISDK",
-            dependencies: [
-                .product(
-                    name: "OnMobileGamificationCore",
-                    package: "OnMobileGamificationCore"
-                ),
-            ],
-        ),
     ]
 )
 
