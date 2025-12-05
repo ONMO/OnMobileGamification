0629898e5bf33abb337628ab1556e26e9e12dc68
Naresh Babu Kommana <naresh.kommana@onmobile.com>
2025-12-05 23:36:14 +0530

    Wrapper Test

--- Package.swift
+++ Package.swift
@@ -9,10 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "OnMobileGamification",
-            targets: [
-                "OnMobileGamificationSDK",
-                "OnMobileGamificationAPISDK"
-            ]
+            targets: ["OnMobileGamificationSDK"]
         ),
     ],
     dependencies: [
@@ -26,6 +23,13 @@ let package = Package(
             name: "OnMobileGamificationSDK",
             url: "https://github.com/ONMO/OnMobileGamification/releases/download/1.0.16/OnMobileGamificationSDK.xcframework.zip",
             checksum: "f22e4522b450c3dc9518827ff4d1f41276b8ca9c64f3ecf3c06be1e6fde46916"
+        ),
+        .target(
+            name: "OnMobileGamificationSDKWrapper",
+            dependencies: [
+                "OnMobileGamificationSDK",
+                .product(name: "OnMobileGamificationCore", package: "OnMobileGamificationCore")
+            ]
         ),
     ]
 )
---
+++ Sources/OnMobileGamificationSDKWrapper/Placeholder.swift
@@ -0,0 +1,6 @@
+//
+//  Placeholder.swift
+//  OnMobileGamification
+//
+//  Created by Naresh Babu Kommana on 05/12/25.
+//
