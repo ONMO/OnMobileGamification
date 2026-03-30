db2ba07e19d6f158e6fe6af71998ec0ea962b8af
Naresh Babu Kommana <naresh.kommana@onmobile.com>
2026-03-30 17:38:12 +0530

    [Update] : Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OnMobileGamificationSDK",
-            url: "https://github.com/ONMO/OnMobileGamification/releases/download/1.0.16/OnMobileGamificationSDK.xcframework.zip",
-            checksum: "f22e4522b450c3dc9518827ff4d1f41276b8ca9c64f3ecf3c06be1e6fde46916"
+            url: "https://github.com/ONMO/OnMobileGamification/releases/download/1.0.17/OnMobileGamificationSDK.xcframework.zip",
+            checksum: "dfd5879519350137b920419d6f30a43c8f36683825e420295b12dda2ca23b0af"
         ),
         .target(
             name: "OnMobileGamificationWrapper",
